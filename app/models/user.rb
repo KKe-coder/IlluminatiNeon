d552ad5e8d77c4599417aab24a4ba49661f53e4c
KKe-coder <carumag726@gmail.com>
2021-01-06 11:03:08 +0000

    Userにis_deletedカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,7 @@ class User < ApplicationRecord
    福岡県:40,佐賀県:41,長崎県:42,熊本県:43,大分県:44,宮崎県:45,鹿児島県:46,
    沖縄県:47
   }
+
+  enum is_deleted: { 有効: false, 退会済: true }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_104927) do
+ActiveRecord::Schema.define(version: 2021_01_06_110034) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_01_06_104927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "residence", default: 0
+    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
