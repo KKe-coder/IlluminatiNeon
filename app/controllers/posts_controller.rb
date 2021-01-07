6a5e438c1750256c0b8dd6d9be6881e966d4928c
KKe-coder <carumag726@gmail.com>
2021-01-07 11:18:18 +0000

    Raty実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -14,7 +16,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to posts_path
+    redirect_to post_path(@post.id)
   end
 
   def edit
