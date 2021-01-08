41e367262d9d8801529f1b06b5d7fd32d7342601
KKe-coder <carumag726@gmail.com>
2021-01-08 09:52:15 +0000

    [Add] 問い合わせ、Mailer実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_073307) do
+ActiveRecord::Schema.define(version: 2021_01_08_092942) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_073307) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "favorites", force: :cascade do |t|
