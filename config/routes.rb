a6b76be9b6297124d2fa9119905acecd5afdfc2a
KKe-coder <carumag726@gmail.com>
2021-01-08 10:56:52 +0000

    退会済ユーザー処理

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'homes/top'
   get 'homes/about'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   root to: 'homes#top'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
 
   resources :users, only: [:show, :edit, :update] do
     resources :murmurs, only: [:create, :destroy]
@@ -21,6 +29,5 @@ Rails.application.routes.draw do
 
   get '/unsubscribe', to: 'users#unsubscribe', as: 'unsubscribe'
   patch '/unsubscribe', to: 'users#withdraw', as: 'withdraw'
-
 
 end
