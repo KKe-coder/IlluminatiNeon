54f4f1d2ef1aa6c99eebdc007d50a967c704b008
KKe-coder <carumag726@gmail.com>
2021-01-09 07:27:55 +0000

    [Add] ゲストユーザーログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get '/about', to: 'homes#about'
+
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
     get "sign_out", :to => "users/sessions#destroy"
+    post "users/guest_sign_in", to: "users/sessions#new_guest"
   end
 
   resources :users, only: [:show, :edit, :update] do
