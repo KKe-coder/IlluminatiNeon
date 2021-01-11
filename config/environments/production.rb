60da33e3d90302c7e53e291f28fbc5b982d1d2d3
KKe-coder <carumag726@gmail.com>
2021-01-11 15:50:10 +0000

    [Update] Mailer設定ファイル

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # mailer settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_ID'],
+    password:             ENV['GMAIL_PW'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
 end
