afea49673c0fbefcc7a926e56d4b4be8b327e953
KKe-coder <carumag726@gmail.com>
2021-01-15 08:07:26 +0000

    [Add] のれんページ

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -22,7 +22,7 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_to root_path
+    redirect_to user_path(current_user.id)
   end
 
   def after_sign_in_path_for(resource)
