63469991dcc3db6c39b91557b0cda2880f2e067e
KKe-coder <carumag726@gmail.com>
2021-01-25 06:47:03 +0000

    [Update] Contactモデルテスト追加

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe 'Contactモデルのテスト', type: :model do
+  describe 'バリデーションテスト' do
+    subject { contact.valid? }
+    let(:contact) { build(:contact) }
+    context 'nameカラム' do
+      it 'お名前が空白であれば登録×' do
+        contact.name = ''
+        is_expected.to eq false
+      end
+      it 'お名前が1文字であれば登録◯' do
+        contact.name = Faker::Lorem.characters(number:1)
+        is_expected.to eq true
+      end
+      it 'お名前が10文字であれば登録◯' do
+        contact.name = Faker::Lorem.characters(number:10)
+        is_expected.to eq true
+      end
+      it 'お名前が11文字であれば登録×' do
+        contact.name = Faker::Lorem.characters(number:11)
+        is_expected.to eq false
+      end
+    end
+    context 'emailカラム' do
+      it 'メールアドレスが空白であれば登録×' do
+        contact.email = ""
+        is_expected.to eq false
+      end
+      it 'メールアドレスが1文字であれば登録◯' do
+        contact.email = Faker::Lorem.characters(number:1)
+        is_expected.to eq true
+      end
+      it 'メールアドレスが30文字であれば登録◯' do
+        contact.email = Faker::Lorem.characters(number:30)
+        is_expected.to eq true
+      end
+      it 'メールアドレスが31文字であれば登録×' do
+        contact.email = Faker::Lorem.characters(number:31)
+        is_expected.to eq false
+      end
+    end
+    context 'messageカラム' do
+      it 'お問い合わせ内容が空白であれば登録×' do
+        contact.message = ""
+        is_expected.to eq false
+      end
+      it 'お問い合わせ内容が1文字であれば登録◯' do
+        contact.message = Faker::Lorem.characters(number:1)
+        is_expected.to eq true
+      end
+      it 'お問い合わせ内容が200文字であれば登録◯' do
+        contact.message = Faker::Lorem.characters(number:200)
+        is_expected.to eq true
+      end
+      it 'お問い合わせ内容が201文字であれば登録×' do
+        contact.message = Faker::Lorem.characters(number:201)
+        is_expected.to eq false
+      end
+    end
+  end
+end
