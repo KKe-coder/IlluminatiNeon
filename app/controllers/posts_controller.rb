9f433e26d147c893b9794516756a578190ad08a4
KKe-coder <carumag726@gmail.com>
2021-02-03 07:40:02 +0000

    [Update] Postコントローラー調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,11 +58,25 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.avgrate = @post.rate
-    if @post.save
-      redirect_to post_path(@post.id)
-    else
-      render :new
-    end
+    dominant_color = Vision.get_image_data(list.image).values
+    colors = { "red" => [220, 53, 69], "orange" => [255, 153, 51], "yellow" => [255, 193, 7], "green" => [40, 167, 69], "blue" => [0, 123, 255], "indigo" => [51, 51, 204], "purple" => [153, 51, 255]}
+    distance = {}
+    colors.each{|key, value|
+      r = "#{(value[0] - dominant_color[0])**2}"
+      g = "#{(value[1] - dominant_color[1])**2}"
+      b = "#{(value[2] - dominant_color[2])**2}"
+      distance[key] = "#{((r + g + b).to_i**(1 / 2.0)).round}"
+      #ここまででColor differenceのHashを作成
+      if key == "purple"
+        sorted_dis = distance.sort {|(k1, v1), (k2, v2)| v1.to_i <=> v2.to_i }.to_h
+        @post.color = sorted_dis.first.first
+          if @post.save
+            redirect_to post_path(@post.id)
+          else
+            render :new
+          end
+      end
+    }
   end
 
   def edit
