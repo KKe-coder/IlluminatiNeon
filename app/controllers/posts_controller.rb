4f94b75792d514842286a8c41f5511776f702550
KKe-coder <carumag726@gmail.com>
2021-02-04 11:14:06 +0000

    [Fix] vision.rbの簡略化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,8 +59,10 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
     @post.avgrate = @post.rate
     if @post.save
-      safesearch = Vision.get_safesearch_data(@post.image)
+      $visionmode = "safesearch"
+      safesearch = Vision.get_image_data(@post.image)
       unless safesearch.value?("LIKELY") || safesearch.value?("VERY_LIKELY") || safesearch.value?("POSSIBLE")
+        $visionmode = "color"
         dominant_color = Vision.get_image_data(@post.image).values
         colors = { "Red" => [220, 53, 69], "Orange" => [255, 153, 51], "Yellow" => [255, 193, 7], "Green" => [40, 167, 69], "Blue" => [0, 123, 255], "Indigo" => [51, 51, 204], "Purple" => [153, 51, 255]}
         distance = {}
--- lib/vision.rb
+++ lib/vision.rb
@@ -21,6 +21,8 @@ module Vision
           },
           features: [
             {
+              type: 'SAFE_SEARCH_DETECTION'
+            },{
               type: 'IMAGE_PROPERTIES'
             }
           ]
@@ -39,45 +41,12 @@ module Vision
       if (error = response_body['responses'][0]['error']).present?
         raise error['message']
       else
-        JSON.parse(response.body)['responses'][0]['imagePropertiesAnnotation']['dominantColors']['colors'].pluck('color')[2]
-      end
-    end
-    def get_safesearch_data(image_file)
-      # APIのURL作成
-      api_url = "https://vision.googleapis.com/v1/images:annotate?key=#{ENV['GOOGLE_API_KEY']}"
-      # 画像をbase64にエンコード※本番環境と開発環境での条件分岐を行う。
-      if Rails.env.production?
-        base64_image = Base64.encode64(open("https://illuminatineon.s3-ap-northeast-1.amazonaws.com/store/#{image_file.id}").read)
-      else
-        base64_image = Base64.encode64(open("#{Rails.root}/public/uploads/#{image_file.id}").read)
-      end
-      # APIリクエスト用のJSONパラメータ
-      params = {
-        requests: [{
-          image: {
-            content: base64_image
-          },
-          features: [
-            {
-              type: 'SAFE_SEARCH_DETECTION'
-            }
-          ]
-        }]
-      }.to_json
-
-      # Google Cloud Vision APIにリクエスト
-      uri = URI.parse(api_url)
-      https = Net::HTTP.new(uri.host, uri.port)
-      https.use_ssl = true
-      request = Net::HTTP::Post.new(uri.request_uri)
-      request['Content-Type'] = 'application/json'
-      response = https.request(request, params)
-      response_body = JSON.parse(response.body)
-      # APIレスポンス出力
-      if (error = response_body['responses'][0]['error']).present?
-        raise error['message']
-      else
-        JSON.parse(response.body)['responses'][0]["safeSearchAnnotation"]
+        binding.pry
+        unless $visionmode == "color"
+          JSON.parse(response.body)['responses'][0]["safeSearchAnnotation"]
+        else
+          JSON.parse(response.body)['responses'][0]['imagePropertiesAnnotation']['dominantColors']['colors'].pluck('color')[2]
+        end
       end
     end
   end
